0871808dba5873dc7f1296f895f7f5a90e4b0526
hitesh_kumar <hitesh_kumar@calypso.com>
2022-08-12 18:47:47 +0200

    first commit
    Change-Id: I9e250fec2b60b1f15289875621a5ed5e28d63e89

---
+++ src/main/java/oliviaproject/hibernate/UserName.java
@@ -0,0 +1,87 @@
+package oliviaproject.hibernate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.FetchType;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.OneToOne;
+import jakarta.persistence.PrimaryKeyJoinColumn;
+
+@Entity
+public class UserName  extends DefaultEntity {
+	@Id
+    @GeneratedValue
+
+	Integer id;
+
+
+	@Column(unique = true)
+	private String userName;
+	private String email;
+	private String phone;
+
+	
+	private Boolean isConnected;
+	private String password;
+    @OneToOne (fetch = FetchType.LAZY, cascade = jakarta.persistence.CascadeType.ALL)
+    @PrimaryKeyJoinColumn(name="PREFERENCE_ID")
+    private ChessBoardPreference preference;
+
+	public ChessBoardPreference getPreference() {
+		return preference;
+	}
+
+	public void setPreference(ChessBoardPreference preference) {
+		this.preference = preference;
+	}
+	public Integer getId() {
+		return id;
+	}
+
+	public void setId(Integer id) {
+		this.id = id;
+	}
+	public String getUserName() {
+		return userName;
+	}
+
+	public void setUserName(String userName) {
+		this.userName = userName;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getPassword() {
+		return password;
+	}
+
+	public void setPassword(String password) {
+		this.password = password;
+	}
+
+	public Boolean getIsConnected() {
+		return isConnected;
+	}
+
+	public void setIsConnected(Boolean isConnected) {
+		this.isConnected = isConnected;
+	}
+
+
+
+}
---
+++ src/test/java/oliviaproject/hibernate/HibernateTest.java
@@ -0,0 +1,94 @@
+package oliviaproject.hibernate;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import junit.framework.TestCase;
+
+public class HibernateTest extends TestCase {
+	private static final Logger log = LoggerFactory.getLogger(HibernateTest.class);
+
+	private static SessionFactory factory;
+
+	public void setUp() {
+		try {
+
+			factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+			;
+		} catch (Throwable ex) {
+			log.error("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+
+	public static void testOneToOneNotCommitted() {
+		Session session = factory.openSession();
+		Transaction tx = null;
+
+		ChessBoardPreference preference = new ChessBoardPreference();
+		preference.setChesswidth(80);
+
+// Add new username object
+		UserName userName = new UserName();
+		userName.setEmail("demo-user@mail.com");
+		userName.setUserName("olivia");
+
+// Save preference
+		session.persist(preference);
+
+		assertNotNull(preference.getId());
+
+// Save username
+		userName.setPreference(preference);
+		session.persist(userName);
+		assertNotNull(userName.getId());
+
+		assertNotNull(preference.getId());
+	}
+
+	public static void testOneToOneAndCommitTx() {
+		Session session = factory.openSession();
+		/**
+		 * 
+		 * tx is needed to commit to database
+		 */
+		Transaction tx = session.beginTransaction();
+
+		ChessBoardPreference preference = new ChessBoardPreference();
+		preference.setChesswidth(80);
+
+		UserName userName = new UserName();
+		userName.setEmail("demo-user@mail.com");
+		userName.setUserName("olivia");
+
+		session.persist(preference);
+
+		assertNotNull(preference.getId());
+
+		userName.setPreference(preference);
+		/**
+		 * Query is not fired here.Object is made persistent. Data for user now
+		 * available in first level cache but not in db.
+		 */
+		session.persist(userName);
+		/**
+		 * 
+		 * So, it is always better to use Persist() rather than Save() as save has to be
+		 * carefully used when dealing with Transient object
+		 */
+		assertNotNull(userName.getId());
+
+		assertNotNull(preference.getId());
+		/**
+		 * Query will be fired at this point and data for user will now also be
+		 * available in DB
+		 * 
+		 */
+		session.getTransaction().commit();
+		session.close();
+	}
+}

28498b140faa18dbcc95b6d539e47f465d59a588
hitesh_kumar <hitesh_kumar@calypso.com>
2022-08-12 18:51:16 +0200

    cleanup
    Change-Id: I9750454d7355133135c4bbeeb1eb5d24abf6b531

--- src/test/resources/oliviaproject/hibernate/database-schama.sql
+++ src/test/resources/oliviaproject/hibernate/database-schama.sql
@@ -1,22 +1,5 @@
-create table author (
-    id bigint not null,
-    first_name varchar(255),
-    last_name varchar(255),
-    primary key (id)
-)
+drop table username;
+drop table chessboardpreference;
 
-create table book (
-    id bigint not null,
-    title varchar(255),
-    author_id bigint,
-    primary key (id)
-)
-
-alter table book
-   add constraint uk_book_title_author
-   unique (title, author_id)
-
-alter table book
-   add constraint fk_book_author_id
-   foreign key (author_id)
-   references author
+select * from username;
+select * from chessboardpreference;

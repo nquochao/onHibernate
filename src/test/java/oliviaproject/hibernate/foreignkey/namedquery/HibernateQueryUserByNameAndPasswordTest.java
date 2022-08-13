a4d6923a815af82420fa25579dd34b20f4e6d3cf
hitesh_kumar <hitesh_kumar@calypso.com>
2022-08-13 13:32:08 +0200

    adding sample for named queries
    Change-Id: I9e3f4c501a80144b9a76744cb1bf90a056569b7e

---
+++ src/test/java/oliviaproject/hibernate/foreignkey/namedquery/HibernateQueryUserByNameAndPasswordTest.java
@@ -0,0 +1,108 @@
+package oliviaproject.hibernate.foreignkey.namedquery;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import jakarta.persistence.Query;
+import junit.framework.TestCase;
+
+public class HibernateQueryUserByNameAndPasswordTest extends TestCase {
+	private static final Logger log = LoggerFactory.getLogger(HibernateQueryUserByNameAndPasswordTest.class);
+
+	private static SessionFactory factory;
+
+	public void setUp() {
+		try {
+
+			factory = new Configuration().configure("hibernate.cfg.xml").buildSessionFactory();
+			;
+		} catch (Throwable ex) {
+			log.error("Failed to create sessionFactory object." + ex);
+			throw new ExceptionInInitializerError(ex);
+		}
+	}
+	
+ 
+	public static void testOneToOneAndCommitTx() {
+		Session session = factory.openSession();
+		/**
+		 * 
+		 * tx is needed to commit to database
+		 */
+		Transaction tx = session.beginTransaction();
+
+		NamedQueryChessBoardPreference preference = new NamedQueryChessBoardPreference();
+		preference.setChesswidth(80);
+
+		NamedQueryUserName userName = new NamedQueryUserName();
+		userName.setEmail("demo-user@mail.com");
+		userName.setUserName("olivia");
+		userName.setPassword("olivia");
+
+		session.persist(preference);
+
+		assertNotNull(preference.getId());
+
+		userName.setPreference(preference);
+		/**
+		 * Query is not fired here.Object is made persistent. Data for user now
+		 * available in first level cache but not in db.
+		 */
+		session.persist(userName);
+		/**
+		 * 
+		 * So, it is always better to use Persist() rather than Save() as save has to be
+		 * carefully used when dealing with Transient object
+		 */
+		assertNotNull(userName.getId());
+
+		assertNotNull(preference.getId());
+		/**
+		 * Query will be fired at this point and data for user will now also be
+		 * available in DB
+		 * 
+		 */
+		session.getTransaction().commit();
+		/**
+		 * 
+		 * if you play twice you need to change the userName Caused by:
+		 * java.sql.SQLIntegrityConstraintViolationException: ORA-00001: unique
+		 * constraint (CALYPSO_161.UK_APNNTI2FQ7NWY851I8QQATYT7) violated This is
+		 * because {link UserName.userName gets the annotation: @Column(unique = true)
+		 *
+		 */
+
+		session.close();
+	}
+	   public static void testQueryUser() {
+	    	Session session=factory.openSession();
+	         Query query=session.createQuery("from NamedQueryUserName where userName=:userName and password=:password");
+	    query.setParameter("userName", "olivia");
+	        query.setParameter("password", "olivia");
+	        NamedQueryUserName user=(NamedQueryUserName)((org.hibernate.query.Query) query).uniqueResult();
+	    if(user!=null) {
+	       System.out.println("username and password are valid");
+	    }else {
+	       System.out.println("username and password are not valid");
+	    }
+	    session.close();
+	}
+	   public static void testQueryUserByNamedQuery() {
+	    	Session session=factory.openSession();
+	         Query query = session.createNamedQuery("checkCredentials", 
+	        		 NamedQueryUserName.class);
+	        		query.setParameter("userName", "olivia");
+	        		query.setParameter("password", "olivia");
+	        		NamedQueryUserName user = (NamedQueryUserName) query.getSingleResult();
+	    if(user!=null) {
+	       System.out.println("username and password are valid");
+	    }else {
+	       System.out.println("username and password are not valid");
+	    }
+	    session.close();
+	}
+}
